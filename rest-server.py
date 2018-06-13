24ff3c8d5507046da89a8c659a5a2f16e1451008
benalloway <benalloway@gmail.com>
2018-06-13 17:35:00 -0400

    added in new route of programming, creating a products API, keeping it simple with Jinja2 templating instead of going angular.js or anything. following a new tutorial

--- rest-server.py
+++ rest-server.py
@@ -1,9 +1,74 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask import Flask, jsonify, abort, make_response, request, url_for, render_template
 from flask_httpauth import HTTPBasicAuth
 auth = HTTPBasicAuth()
 
 app = Flask(__name__)
+
+# Memory Data
+# TASKSDATA = {
+# 	{
+# 		'id': 1,
+# 		'title': 'Buy groceries',
+# 		'description': 'Milk, Cheese, Pizza, Fruit, Tyleno',
+# 		'done': False
+# 	},
+# 	{
+# 		'id': 2,
+# 		'title': 'Learn Python',
+# 		'description': 'Need to find a good Python tutorial on the web',
+# 		'done': False
+# 	},
+# 	{
+# 		'id': 3,
+# 		'title': 'Wedding',
+# 		'description': 'wine, food, beer, apps, minister',
+# 		'done': False
+# 	}
+# }
+
+PRODUCTS = {
+    'iphone': {
+        'name': 'iPhone 5S',
+        'category': 'Phones',
+        'price': 699,
+    },
+    'galaxy': {
+        'name': 'Samsung Galaxy 5',
+        'category': 'Phones',
+        'price': 649,
+    },
+    'ipad-air': {
+        'name': 'iPad Air',
+        'category': 'Tablets',
+        'price': 649,
+    },
+    'ipad-mini': {
+        'name': 'iPad Mini',
+        'category': 'Tablets',
+        'price': 549
+    }
+}
+
+# Index.HTML
+@app.route('/')
+@app.route('/home/')
+def index():
+	return render_template('index.html', products=PRODUCTS)
+
+@app.route('/product/<key>')
+def detail(key):
+	product = PRODUCTS.get(key)
+	if not product:
+		abort(404)
+	return render_template('detail.html', product=product)
+
+
+
+# 
+# RESTful API stuff
+# 
+
 
 # Memory Data
 tasks = [
@@ -27,6 +92,8 @@ tasks = [
 	}
 ]
 
+
+
 # Authentication, add "@auth.login_required" where needed
 @auth.get_password
 def get_password(username):
@@ -36,6 +103,7 @@ def get_password(username):
 @auth.error_handler
 def unauthorized():
 	return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
 
 # returns public uri instead of id
 def make_public_task(task):
@@ -47,14 +115,17 @@ def make_public_task(task):
 			new_task[field] = task[field]
 	return new_task
 
+
 # GET list of tasks 
 @app.route('/todo/api/v1.0/tasks/', methods=['GET'])
 @auth.login_required
 def get_tasks():
 	return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
+
 # POST new list data
 @app.route('/todo/api/v1.0/tasks/', methods=['POST'])
+@auth.login_required
 def create_task():
 	if not request.json or not 'title' in request.json:
 		abort(400)
@@ -67,16 +138,20 @@ def create_task():
 	tasks.append(task)
 	return jsonify({'task': task}), 201
 
+
 # GET individual task by id
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+@auth.login_required
 def get_task(task_id):
 	task = [task for task in tasks if task['id'] == task_id]
 	if len(task) == 0:
 		abort(404)
 	return jsonify({'task':task[0]})
 
+
 # PUT, update individual task by id
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+@auth.login_required
 def update_task(task_id):
 	task = [task for task in tasks if task['id'] == task_id]
 	if len(task) == 0:
@@ -94,8 +169,10 @@ def update_task(task_id):
 	task[0]['done'] = request.json.get('done', task[0]['done'])
 	return jsonify({'task':task[0]})
 
+
 # DELETE, individual task by id
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+@auth.login_required
 def delete_task(task_id):
 	task = [task for task in tasks if task['id'] == task_id]
 	if len(task) == 0:
