3bfd799a44397f048d487a95e13045ff34e90f5b
benalloway <benalloway@gmail.com>
2018-06-15 11:17:16 -0400

    created functions to write to db, read from db, and handle CRUD requests. working on routes now.

--- rest-server.py
+++ rest-server.py
@@ -8,56 +8,96 @@ auth = HTTPBasicAuth()
 app = Flask(__name__)
 
 # Memory Data
-PRODUCTS = [
-    {
-        'name': 'iPhone 5S',
-        'category': 'Phones',
-        'price': 699,
-        'id': 1
-    },
-    {
-        'name': 'Samsung Galaxy 5',
-        'category': 'Phones',
-        'price': 649,
-        'id': 2
-    },
-    {
-        'name': 'iPad Air',
-        'category': 'Tablets',
-        'price': 649,
-        'id': 3
-    },
-    {
-        'name': 'iPad Mini',
-        'category': 'Tablets',
-        'price': 549,
-        'id': 4
-    }
-]
+PRODUCTS = {
+	'1': {
+	    'name': 'iPhone 5S',
+	    'category': 'Phones',
+	    'price': 699,
+	    'id': 1
+	},
+	'2': {
+	    'name': 'Samsung Galaxy 5',
+	    'category': 'Phones',
+	    'price': 649,
+	    'id': 2
+	},
+	'3': {
+	    'name': 'iPad Air',
+	    'category': 'Tablets',
+	    'price': 649,
+	    'id': 3
+	},
+	'4': {
+	    'name': 'iPad Mini',
+	    'category': 'Tablets',
+	    'price': 549,
+	    'id': 4
+	}	
+}
 
 # 
-# DB tool
+# DB tools
+# Version 1.0 
+# description: version 1.0 is function based interaction with DB. version 1.1 will be updated to class based interaction with DB
 # 
 
-def dbwrite(args):
+def writeDB(args=None):
 	with open('database/products.csv', 'w') as dbfile:
 		fieldnames = ['name', 'category', 'price', 'id']
 		writer = csv.DictWriter(dbfile, fieldnames=fieldnames)
-
 		writer.writeheader()
-		for product in args:
-			writer.writerow(product)
+		for k,v in args.iteritems():
+			writer.writerow(v)
 
-dbwrite(PRODUCTS)
 
+def getDB(args=None):
+	with open('database/products.csv') as dbfile:
+		reader = csv.DictReader(dbfile)
+		db = {}
+		for row in reader:
+			db[row['id']] = {'name':row['name'], 'category':row['category'], 'price':row['price'], 'id':row['id']}
+		return db
+
+
+def deleteDB(args=None):
+	if args is not None:
+		db = getDB()
+		if args in db:
+			del db[args]
+			writeDB(db)
+
+
+def postDB(args=None):
+	if args is not None:
+		db = getDB()
+		data = args
+		ids = []
+		for k,v in db.iteritems():
+			ids.append(v['id'])
+		ids = [int(x) for x in ids]
+		ids = max(ids) + 1
+		data['id'] = str(ids)
+		db[data['id']] = data
+		writeDB(db)	
+
+
+def putDB(args=None):
+	if args is not None:
+		db = getDB()
+		if args['id'] in db:
+			db[args['id']] = args
+			writeDB(db)
 
 # 
 # List of Products, home page
 # 
+
 @app.route('/')
 @app.route('/home/')
 def index():
-	return render_template('index.html', products=PRODUCTS)
+	products = getDB()
+	products = sorted(products)
+	return render_template('index.html', products=products)
 
 
 # 
@@ -66,7 +106,7 @@ def index():
 
 @app.route('/product/<key>')
 def detail(key):
-	product = PRODUCTS.get(key)
+	product = getDB().get(key)
 	if not product:
 		abort(404)
 	return render_template('detail.html', product=product)
